3cd8f3ae58d9bc792f9719971a1630dc1dd2500d
mat <ivandaev9@gmail.com>
2023-12-08 17:46:33 -0500

    modified Policy.tsx

--- src/components/Policy.tsx
+++ src/components/Policy.tsx
@@ -4,7 +4,7 @@ import {Link} from "@nextui-org/react";
 export const Policy = () => {
     return (
         <p className="text-xs">
-            Нажимая на кнопку, вы соглашаетесь с
+            <div>Нажимая на кнопку, вы соглашаетесь с</div>
             <Link href={'#'} className={'cursor-pointer hover:text-gray-300 text-xs underline'}>политикой конфиденциальности</Link>
         </p>
     );
