4aebd3eb35420aed6f3a7efc221e95065e6591f1
mat <ivandaev9@gmail.com>
2023-12-08 16:57:06 -0500

    Added Offer block.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+import {Offer} from "@/components/Offer";
 
 export default function Home() {
   return (
    <main>
-     <h1>Home</h1>
+       <Offer/>
+       {/*review*/}
+       {/*portfolio*/}
+       {/*price-callback*/}
+       {/*steps*/}
+       {/*types*/}
+       {/*discount-callback*/}
+       {/*faq*/}
+       {/*time*/}
+       {/*reviews*/}
+       {/*convenient*/}
+       {/*advantages*/}
+       {/*payments*/}
+       {/*rooms*/}
+       {/*service-price*/}
+       {/*ask-callback*/}
+       {/*text-block*/}
+       {/*tags*/}
+       {/*prices*/}
    </main>
   )
 }
