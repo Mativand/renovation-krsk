f68f1f3acb80397d58f4ec137b00821df6bcaf9b
mat <ivandaev9@gmail.com>
2023-12-09 10:40:06 -0500

    Fixed nested div in p

--- src/components/Policy.tsx
+++ src/components/Policy.tsx
@@ -3,9 +3,9 @@ import {Link} from "@nextui-org/react";
 
 export const Policy = () => {
     return (
-        <p className="text-xs">
+        <div className="text-xs">
             <div>Нажимая на кнопку, вы соглашаетесь с</div>
             <Link href={'#'} className={'cursor-pointer hover:text-gray-300 text-xs underline'}>политикой конфиденциальности</Link>
-        </p>
+        </div>
     );
 };
--- src/components/features/Call.tsx
+++ src/components/features/Call.tsx
@@ -5,12 +5,12 @@ import {PHONE_NUMBER, SCHEDULE} from "@/components/shared/consts";
 export const Call = () => {
     return (
         <div>
-            <p className={'relative'}>
+            <div className={'relative'}>
                 <div className={'absolute text-yellow-300 top-0 -left-8'}>
                     <IoPhonePortraitOutline size="1.7em"/>
                 </div>
                 <span className={'text-xl'}>{PHONE_NUMBER}</span>
-            </p>
+            </div>
             <p className={'text-xs text-gray-300'}>{SCHEDULE}</p>
         </div>
     );
