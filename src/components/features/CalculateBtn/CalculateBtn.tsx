35b57ef8fe49f3cee594ba5a0c0f7467d79ede3f
mat <ivandaev9@gmail.com>
2023-12-09 13:56:31 -0500

    added style module for CalculateBtn

--- src/components/features/CalculateBtn/CalculateBtn.tsx
+++ src/components/features/CalculateBtn/CalculateBtn.tsx
@@ -1,12 +1,13 @@
 import {FC} from 'react';
 import {Button, Link} from "@nextui-org/react";
 import { SlCalculator } from "react-icons/sl";
+import s from './styles.module.scss';
 
 export const CalculateBtn: FC<{label: string}> = ({label}) => {
     return (
-        <Button as={Link} color="warning" href="#" variant="flat">
+        <Button className={s.btn} as={Link} color="warning" href="#" variant="flat">
             <SlCalculator size={'1.8em'}/>
-            {label}
+            <span className={s.label}>{label}</span>
         </Button>
     );
 };
